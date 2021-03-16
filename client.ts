ca41ff11694ed76d379169540e9d5c3bf2a5c392
Anastasia Borissova <aborisso@ualberta.ca>
2021-03-15 20:03:11 -0400

    Changed port

--- client.ts
+++ client.ts
@@ -10,7 +10,7 @@ interface ICoords {
 
 class GameScene extends Phaser.Scene {
   private HOST = window.location.hostname; // localhost and 127.0.0.1 handled
-  private PORT = 8080; // change this if needed
+  private PORT = 8077; // change this if needed
 
   private wsClient?: WebSocket;
   private sprite?: Phaser.GameObjects.Sprite;
